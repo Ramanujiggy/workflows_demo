d3dd8f1e2de779ee68756c727d56362d1dd843b4
Rob <robertdgelin@hotmail.com>
2023-11-29 20:11:14 -0600

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -4,7 +4,7 @@ def main():
     print("Hello world! From Python: " +str(sys.version_info))
     if sys.version_info >= (3,6) and sys.version_info < (3,7):
         #lets make this script fail for python 3.6
-        raise Exception('Python version 3.6x is unsupported!")
+        raise Exception('Python version 3.6x is unsupported!')
 
 
 
